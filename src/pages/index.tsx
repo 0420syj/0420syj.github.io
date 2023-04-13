3934cbee3d3b29b317805e40a186d07c31f44173
Wan Sim <0420syj@naver.com>
2023-04-14 05:42:05 +0900

    Fix : 메인페이지 영어 번역추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Translate, { translate } from "@docusaurus/Translate";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
@@ -9,6 +10,19 @@ import styles from "./index.module.css";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
+
+  siteConfig.title = translate({
+    id: "title",
+    message: siteConfig.title,
+    description: "The title of the website",
+  });
+
+  siteConfig.tagline = translate({
+    id: "tagline",
+    message: siteConfig.tagline,
+    description: "The title of the website",
+  });
+
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
@@ -16,7 +30,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link className="button button--secondary button--lg" to="/blog">
-            블로그 📗
+            <Translate>블로그 📗</Translate>
           </Link>
         </div>
       </div>
