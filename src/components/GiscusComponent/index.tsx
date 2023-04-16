7d2c61bcbe14e68955460a5531b693c6aef2417a
Wan Sim <0420syj@naver.com>
2023-04-16 18:08:48 +0900

    Refactor : 댓글 컴포넌트
    DocusaurusContextType 타입 설정, 하드코딩 추출

--- src/components/GiscusComponent/index.tsx
+++ src/components/GiscusComponent/index.tsx
@@ -9,6 +9,11 @@ interface GiscusProps {
   categoryId: string;
 }
 
+type DocusaurusContextType = {
+  siteConfig: { customFields: GiscusProps };
+  i18n: { currentLocale: string };
+};
+
 const GiscusComponent: React.FC = () => {
   const { colorMode } = useColorMode();
 
@@ -17,19 +22,19 @@ const GiscusComponent: React.FC = () => {
       customFields: { repoId, category, categoryId },
     },
     i18n: { currentLocale },
-  } = useDocusaurusContext() as unknown as {
-    siteConfig: { customFields: GiscusProps };
-    i18n: { currentLocale: string };
-  };
+  } = useDocusaurusContext() as unknown as DocusaurusContextType;
+
+  const repo = "0420syj/0420syj.github.io";
+  const term = "Welcome to Wanny's Blog";
 
   return (
     <Giscus
-      repo="0420syj/0420syj.github.io"
+      repo={repo}
       repoId={repoId}
       category={category}
       categoryId={categoryId}
       mapping="url"
-      term="Welcome to Wanny's Blog"
+      term={term}
       strict="0"
       reactionsEnabled="1"
       emitMetadata="1"
